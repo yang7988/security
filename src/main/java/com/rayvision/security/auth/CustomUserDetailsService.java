111582b0b251ea7b5a1bc3ca61d49804246a23d6
xuyangyang <xuyangyang@rayvision.com>
2017-11-10 11:37:11 +0800

    security-admin

--- src/main/java/com/rayvision/security/auth/CustomUserDetailsService.java
+++ src/main/java/com/rayvision/security/auth/CustomUserDetailsService.java
@@ -3,12 +3,12 @@ package com.rayvision.security.auth;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by admin on 2017/11/9.
  */
-@Service
+@Component
 public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
--- src/main/java/com/rayvision/security/config/SecurityConfiguration.java
+++ src/main/java/com/rayvision/security/config/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package com.rayvision.security.config;
 
 import com.rayvision.security.auth.CustomAuthenticationProvider;
+import com.rayvision.security.auth.RestAuthenticationEntryPoint;
+import com.rayvision.security.auth.RestAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,6 +10,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 /**
  * Created by admin on 2017/11/9.
@@ -18,7 +21,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private CustomAuthenticationProvider customAuthenticationProvider;
-
+    @Autowired
+    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+    @Autowired
+    private RestAuthenticationSuccessHandler restAuthenticationSuccessHandler;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(customAuthenticationProvider);
@@ -26,7 +32,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.antMatcher("/api/**")
-                .authorizeRequests().anyRequest().permitAll();
+            http.csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/foos").authenticated()
+                .and()
+                .formLogin()
+                .successHandler(restAuthenticationSuccessHandler)
+                .failureHandler(new SimpleUrlAuthenticationFailureHandler())
+                .and()
+                .logout();
     }
 }
