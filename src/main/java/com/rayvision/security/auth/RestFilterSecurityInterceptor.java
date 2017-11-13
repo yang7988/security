20b0b033311544282230a343b267c70038b72b5d
xuyangyang <xuyangyang@rayvision.com>
2017-11-13 14:40:23 +0800

    security-admin

---
+++ src/main/java/com/rayvision/security/auth/RestFilterSecurityInterceptor.java
@@ -0,0 +1,59 @@
+package com.rayvision.security.auth;
+
+import org.springframework.security.access.SecurityMetadataSource;
+import org.springframework.security.access.intercept.AbstractSecurityInterceptor;
+import org.springframework.security.access.intercept.InterceptorStatusToken;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by admin on 2017/11/13.
+ */
+@Component
+public class RestFilterSecurityInterceptor extends AbstractSecurityInterceptor implements Filter {
+
+    private FilterInvocationSecurityMetadataSource securityMetadataSource;
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        FilterInvocation fi = new FilterInvocation( request, response, chain );
+        invoke(fi);
+    }
+
+    public FilterInvocationSecurityMetadataSource getSecurityMetadataSource(){
+        return this.securityMetadataSource;
+    }
+
+    public Class<? extends Object> getSecureObjectClass(){
+        return FilterInvocation.class;
+    }
+
+
+    public void invoke( FilterInvocation fi ) throws IOException, ServletException{
+        InterceptorStatusToken token = super.beforeInvocation(fi);
+        try{
+            fi.getChain().doFilter(fi.getRequest(), fi.getResponse());
+        }finally{
+            super.afterInvocation(token, null);
+        }
+    }
+
+
+    @Override
+    public SecurityMetadataSource obtainSecurityMetadataSource(){
+        return this.securityMetadataSource;
+    }
+
+
+    public void setSecurityMetadataSource(FilterInvocationSecurityMetadataSource securityMetadataSource){
+        this.securityMetadataSource = securityMetadataSource;
+    }
+
+    public void destroy(){}
+
+    public void init( FilterConfig filterconfig ) throws ServletException{}
+}
---
+++ src/main/java/com/rayvision/security/auth/RestInvocationSecurityMetadataSource.java
@@ -0,0 +1,76 @@
+package com.rayvision.security.auth;
+
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.SecurityConfig;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+/**
+ * Created by admin on 2017/11/13.
+ */
+@Component
+public class RestInvocationSecurityMetadataSource implements FilterInvocationSecurityMetadataSource {
+
+//    private UrlMatcher urlMatcher = new AntUrlPathMatcher();
+
+    private static Map<String, Collection<ConfigAttribute>> resourceMap = null;
+
+    public RestInvocationSecurityMetadataSource() {
+        loadResourceDefine();
+    }
+
+    private void loadResourceDefine() {
+//        查询当前用户拥有的权限
+        List<String> authoritys = null;
+        resourceMap = new HashMap<String, Collection<ConfigAttribute>>();
+
+        for (String auth : authoritys) {
+            ConfigAttribute ca = new SecurityConfig(auth);
+            //通过权限找到用户的资源
+            List<String> resources = null;
+            for (String res : resources) {
+                String url = res;
+                if (resourceMap.containsKey(url)) {
+                    Collection<ConfigAttribute> value = resourceMap.get(url);
+                    value.add(ca);
+                    resourceMap.put(url, value);
+                } else {
+                    Collection<ConfigAttribute> atts = new ArrayList<ConfigAttribute>();
+                    atts.add(ca);
+                    resourceMap.put(url, atts);
+                }
+            }
+        }
+    }
+
+    @Override
+    public Collection<ConfigAttribute> getAllConfigAttributes() {
+        return null;
+    }
+
+    // 根据URL，找到相关的权限配置。
+    @Override
+    public Collection<ConfigAttribute> getAttributes(Object object) throws IllegalArgumentException {
+        String url = ((FilterInvocation) object).getRequestUrl();
+        int firstQuestionMarkIndex = url.indexOf("?");
+        if (firstQuestionMarkIndex != -1) {
+            url = url.substring(0, firstQuestionMarkIndex);
+        }
+        Iterator<String> ite = resourceMap.keySet().iterator();
+        while (ite.hasNext()) {
+            String resURL = ite.next();
+            /*if (urlMatcher.pathMatchesUrl(url, resURL)) {
+                return resourceMap.get(resURL);
+            }*/
+        }
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> arg0) {
+        return true;
+    }
+}
